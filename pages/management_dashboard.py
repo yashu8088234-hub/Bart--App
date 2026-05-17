63c89e4764bdcbe49375ae1dae8621222cb950dc
Yash <yashu8088234@gmail.com>
2026-05-17 12:55:32 +0300

    Update management_dashboard.py

--- pages/management_dashboard.py
+++ pages/management_dashboard.py
@@ -56,7 +56,7 @@ except:
 # BRANCHES
 # =========================================================
 
-@st.cache_data(ttl=600)
+@st.cache_data(ttl=300)
 def load_branches():
     sheet = client.open("MASTERBRANCHSHEET").sheet1
     data = sheet.get_all_records()
