3f357b11d578ef3ad6022eb9c69c7d80865708db
Yash <yashu8088234@gmail.com>
2026-05-20 21:16:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -85,11 +85,11 @@ st.markdown("<p class='animate-text delay-4' style='text-align: center; font-siz
 # =========================================================
 # CARDS
 # =========================================================
-grid_left, grid_right = st.columns(2, gap="medium")
+grid_left, grid_right = st.columns(2, gap="large")
 
 with grid_left:
     st.markdown('<div class="card-wrapper">', unsafe_allow_html=True)
-    st.markdown("<p style='font-size: 20px; font-weight: 700; color: #1E293B; margin-bottom: 4px;'>Staff Control</p>", unsafe_allow_html=True)
+    st.markdown("<p style='font-size: 20px; font-weight: 700; color: #1E293B;margin-top:-10px margin-bottom: 4px;'>Staff Control</p>", unsafe_allow_html=True)
     st.markdown("<p style='font-size: 14px; color: #64748B; margin-bottom: 25px;'>Log daily updates, run item balance checkers, and communicate data parameters.</p>", unsafe_allow_html=True)
     if st.button("Access Staff Control →", use_container_width=True, key="staff_btn"):
         st.switch_page("pages/staff_dashboard.py")
