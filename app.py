5839b4513b7ecf2019bd791d9fbe38c343c3df34
Yash <yashu8088234@gmail.com>
2026-05-25 16:08:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ h1 { margin-bottom: 0px !important; }
     font-weight: 900;
     color: #8B5CF6 !important;    
     position: relative;
-    top: -37px;                   
+    top: -40px;                   
     left: 8px;
     display: inline-block;
     opacity: 1;
