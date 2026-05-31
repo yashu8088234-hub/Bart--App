f22d8757f7547cc1e526604709b4ac51b87052dc
Yash <yashu8088234@gmail.com>
2026-05-31 10:54:32 +0300

    Update staff_schedule.py

--- pages/staff_schedule.py
+++ pages/staff_schedule.py
@@ -10,6 +10,19 @@ from st_aggrid import AgGrid
 
 st.set_page_config(layout="wide", page_title="BART Master Schedule")
 
+
+st.markdown("""
+<style>
+[data-testid="stSidebar"] {
+    display: none;
+}
+
+[data-testid="collapsedControl"] {
+    display: none;
+}
+</style>
+""", unsafe_allow_html=True)
+
 # =========================
 # AUTH CHECK
 # =========================
